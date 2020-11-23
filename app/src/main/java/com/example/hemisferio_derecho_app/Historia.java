6cbd3923ed142e810ee9654a1ea8160139b83a45
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2020-11-23 18:44:45 -0500

    historia

--- app/src/main/java/com/example/hemisferio_derecho_app/Historia.java
+++ app/src/main/java/com/example/hemisferio_derecho_app/Historia.java
@@ -20,7 +20,16 @@ public class Historia extends AppCompatActivity {
 
         Intent intent = getIntent();
         nombre=intent.getStringExtra("nombre");
-        historia="Texto";//Texto de la historia
+        historia="Eran las 8 de la mañana y" +" "+nombre+" "+
+                "como de costumbre se encontraba jugando en el ático. " +
+                "De repente encontró una llave misteriosa que nunca había visto, de color dorado y forma de colibrí, " +
+                "no dudó ni por un instante y comenzó a buscar qué abría, intentó usarla en varias cerraduras, en la puerta " +
+                "de un viejo armario, en el baúl del abuelo, investigo por todo el lugar y vio una puerta de madera con visos dorados y se acercó a " +
+                "ella saltando como un grillo, pensando que la había encontrado, pero cuando intentó abrirla, la llave no entró en la cerradura. " +" "+
+                nombre +" "+
+                "se sentía un poco desilusionada y estaba pensando en rendirse, se sentó en encima de una caja de carton llena de revistas viejas, " +
+                "y desde ahí vio un libro con una pasta de madera tallada con figuras de animales salvajes donde sobresalía un hermoso colibrí que señalaba " +
+                "un cerrojo dorado que impedía abrir el libro.";
         text_historia= findViewById(R.id.text_historia);
         home = findViewById(R.id.home_h);
         imageView_anterior=findViewById(R.id.anterior3);
@@ -45,9 +54,12 @@ public class Historia extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent1 = new Intent(Historia.this,Historia2.class);
+                intent1.putExtra("nombre",nombre);
                 startActivity(intent1);
             }
         });
 
+        text_historia.setText(historia);
     }
+
 }
--- app/src/main/java/com/example/hemisferio_derecho_app/Historia2.java
+++ app/src/main/java/com/example/hemisferio_derecho_app/Historia2.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Historia2 extends AppCompatActivity {
 
     String historia,nombre;
+    TextView historia2;
     ImageView imageView_anterior,home,librito;
 
 
@@ -20,7 +23,20 @@ public class Historia2 extends AppCompatActivity {
         home = findViewById(R.id.home_h2);
         imageView_anterior=findViewById(R.id.anterior4);
         Intent intent = getIntent();
+
         nombre=intent.getStringExtra("nombre");
+        Log.d("hola", "onCreate: "+nombre);
+        historia2= findViewById(R.id.text_historia2);
+        historia="Al introducir la llave en el cerrojo todo el lugar se llenó de una bella luz rosada y en un abrir y cerrar de ojos, estaba rodeada de mucha vegetación y se " +
+                "escuchaba mucho alboroto, tanto que pensaba que estaba en el zoológico, miraba todo a su alrededor con sus ojos abiertos, como los de un búho, no sabía lo que estaba pasando. " +
+                "De repente de las ramas de los árboles empezaron a salir monos, pájaros, ardillas y todo el lugar cambió de color, las hojas verdes de la vegetación, los troncos de los árboles y el suelo se tornaron de azul, " +
+                "el lugar era mágico, al mirar de cerca pudo observar que eso se debía a que todo el lugar estaba lleno de hermosas mariposas azules. Los pájaros llenos de curiosidad volaban alrededor " +
+                "de " +
+                nombre +" "+
+                " tanto que ella sentía las " +
+                "plumas rosar su cuerpo, los monos no se quedaban atrás, saltaban a su lado y eran tan atrevidos que le tocaban la ropa y le olían las manos, las ardillas subían por su cuerpo y trepaban hasta su cabeza, pero al cabo de un tiempo todos se apartaron " +
+                "de ella para dejar pasar al orgulloso pavo real que salió de en medio de un campo de flores rojas, él se acercó confiado con su cabeza en alto, demostrando que era el rey del lugar y" +
+                " en medio de un lindo baile le mostraba el camino que la llevaría a una cascada cristalina que dejaba ver una casa. ";
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +52,7 @@ public class Historia2 extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
+        historia2.setText(historia);
 
 
     }
